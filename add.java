44f838f17d9264ffcb555e86653a1522903c2ec6
Iamomkarcrown <omkaware750@gmail.com>
2025-08-22 09:28:29 -0700

    wrote Multiplication line

--- add.java
+++ add.java
@@ -3,7 +3,9 @@ public class add{
         int a=3;
         int b=4;
         int c= a + b;
+        int d= a * b;
         System.out.println("Result="+c);
         System.out.println(c);
+        System.out.println(d);
     }
 }
